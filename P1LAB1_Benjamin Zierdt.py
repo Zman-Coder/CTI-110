9114aa0ae76b734bf42cf0e1d93b9cc83995553d
Zman-Coder <zierdttb8162@student.faytechcc.edu>
2026-02-08 16:11:51 -0600

    First Python Assignment
    Taking input from user and printing

---
+++ P1LAB1_Benjamin Zierdt.py
@@ -0,0 +1,7 @@
+#Benajamin Zierdt-Thorson
+#P1LAB1.py
+#inpute and Print
+
+first_name = input("Enter your first name:  ")
+last_name = input("Enter your last name:  ")
+print("Hello, " ,first_name,last_name + "!" , "Welcome to CTI-110!")
